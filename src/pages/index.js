1fb14896a4df11d39f15a8a9834528de264200a8
Sudhakar S <sudhakar1470820@gmail.com>
2024-09-28 12:22:04 +0530

    h1 updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,8 @@ export default function Home({ userData }) {
           </Breadcrumbs>
 
           <UserTable userData={userData} />
+
+          <h1>Test</h1>
         </Container>
       </main>
     </>
